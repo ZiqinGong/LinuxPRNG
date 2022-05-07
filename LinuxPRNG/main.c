06c3d054cbc07e689413fab8bb612f16094b714f
Ziqin Gong <gongzq0301@sjtu.edu.cn>
2022-05-07 19:03:31 +0800

    Initial Commit

---
+++ LinuxPRNG/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LinuxPRNG
+//
+//  Created by 龚子钦 on 2022/5/7.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
